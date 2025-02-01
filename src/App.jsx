cb49b9299264f96f86b28864e0b7b5403b9183d6
manueljo <mikebillyy@gmail.com>
2025-02-01 21:56:57 +0100

    add movie fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react"
+import Search from "./components/Search"
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers : {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  }
+}
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const fetchMovies = async () => {
+    try {
+      
+    } catch (error) {
+      setErrorMessage('error fetching movies. Please try again later.');
+    }
+  }
+
+  useEffect(() =>{
+
+  }, [])
 
   return (
   <main>
@@ -7,9 +35,10 @@ function App() {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
+        
       </div>
-
     </div>
   </main>
   )
