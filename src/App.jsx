c181b91262eb79d3bf900d3f786fc0a04cb10da9
manueljo <mikebillyy@gmail.com>
2025-02-02 12:23:49 +0100

    display fetch movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import Search from "./components/Search"
+import Spinner from "./components/Spinner"
+import MovieCard from "./components/MovieCard"
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -13,19 +15,39 @@ const API_OPTIONS = {
 }
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
-      
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if(!response.ok){
+        throw new Error("Error fetching movies");
+      }
+      const data = await response.json();
+      if (data.response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return
+      }
+      setMovieList(data.results || [])
+
     } catch (error) {
+      console.error(error)
       setErrorMessage('error fetching movies. Please try again later.');
+    }finally{
+      setIsLoading(false);
     }
   }
 
   useEffect(() =>{
-
+    fetchMovies();
   }, [])
 
   return (
@@ -37,7 +59,22 @@ function App() {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
-        
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All movies</h2>
+          
+          {/* {errorMessage && <p className="text-red-500">{errorMessage}</p>} */}
+          {isLoading ? (
+            <Spinner/>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </div>
   </main>
