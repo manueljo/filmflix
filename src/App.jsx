57735582be4d8d081394690c8d3547b7f4bf6475
manueljo <mikebillyy@gmail.com>
2025-02-03 13:28:56 +0100

    optimize search with debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { useDebounce } from "react-use"
 import Search from "./components/Search"
 import Spinner from "./components/Spinner"
 import MovieCard from "./components/MovieCard"
@@ -19,6 +20,9 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useDebounce(()=> setDebouncedSearchTerm(searchTerm),500,[searchTerm])
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true);
@@ -47,8 +51,8 @@ function App() {
   }
 
   useEffect(() =>{
-    fetchMovies(searchTerm);
-  }, [searchTerm])
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm])
 
   return (
   <main>
